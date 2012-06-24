55f2d102dda6a366f91341cbd1f804d6e9ec4dbd
Ali Ibrahim <aibrahim2k2@gmail.com>
2012-06-24 15:36:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterVideoPlaylist::Application.config.secret_token = 'a1144b98bcdcb2a1845eaea3401b9aea56a30c31b0fbed500ac21daf44367adbafce0c968c2d5799cdefba0ef4f41e48087c4eb59c0956dfd63e648015076d34'

edc2b03542e4647d4c9d4252a20913a59e07b8f9
Ali Ibrahim <aibrahim2k2@gmail.com>
2012-08-30 22:21:36 -0400

    Use YouTube Iframe API to load players for playlists

--- app/assets/javascripts/backbone/views/playlist_show.js
+++ app/assets/javascripts/backbone/views/playlist_show.js
@@ -1,16 +1,16 @@
 BetterVideoPlaylist.Views.PlaylistShow = Backbone.View.extend({
   render: function() {
-    var iframe = this.make("iframe",
-      {
-	"id": this.model.escape("name"),
-        "type": "text/html",
-	"width": "200",
-	"height": "200",
-	"src": this.model.youtube_url(),
-	"frameborder": "0",
-	"allowfullscreen": ""
-      });
-    this.$el = iframe
     return this;
+  },
+
+  createPlayer: function() {
+    new YT.Player(this.el, {
+      height: '200',
+      width: '200', 
+      playerVars: {
+        'listType': 'playlist',
+    	'list': this.model.escape("youtube_id") 
+      }
+    });
   }
 });
--- app/assets/javascripts/backbone/views/playlists_index.js
+++ app/assets/javascripts/backbone/views/playlists_index.js
@@ -6,7 +6,9 @@ BetterVideoPlaylist.Views.PlaylistsIndex = Backbone.View.extend({
     this.collection.each(function(playlist, index) {
       var playlistShow = 
         new BetterVideoPlaylist.Views.PlaylistShow({ model: playlist });
+
       self.$el.append(playlistShow.render().$el);
+      playlistShow.createPlayer();
 
       if((index + 1) % 3 == 0) {
         self.$el.append("<br/>");

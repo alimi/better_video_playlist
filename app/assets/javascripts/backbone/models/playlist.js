ed97211729d76c1678e1c23e728cdd6fc3fe2e05
Ali Ibrahim <aibrahim2k2@gmail.com>
2012-10-02 21:40:23 -0400

    Play a playlist and it gets bigger

--- app/assets/javascripts/backbone/models/playlist.js
+++ app/assets/javascripts/backbone/models/playlist.js
@@ -6,13 +6,24 @@ BetterVideoPlaylist.Models.Playlist = Backbone.Model.extend({
   },
 
   createApiPlayer: function(containing_element) {
+    var self = this;
+
     return new YT.Player(containing_element, {
       height: '200',
       width: '200', 
       playerVars: {
         'listType': 'playlist',
         'list': this.escape("youtube_id")
+      },
+      events: {
+        'onStateChange': self.playerStateChanged
       }
     });
+  },
+
+  playerStateChanged: function(event) {
+    if(event.data == YT.PlayerState.PLAYING) {
+      event.target.setSize(640, 360);
+    }
   }
 });

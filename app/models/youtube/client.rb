16c7473a5da4c0a7b599dea36155a913d36d9e31
Ali Ibrahim <aibrahim2k2@gmail.com>
2013-03-13 22:01:20 -0400

    Fold Youtube::Authorization into Youtube::Client

--- app/models/youtube/client.rb
+++ app/models/youtube/client.rb
@@ -16,5 +16,19 @@ module Youtube
       @client.authorization.scope =
         'https://www.googleapis.com/auth/youtube'
     end
+
+    def authorization_redirect_url
+      @client.authorization.authorization_uri.to_s
+    end
+
+    def generate_tokens(authorization_code)
+      @client.authorization.code = authorization_code
+      response = @client.authorization.fetch_access_token!
+
+      {
+        :access_token => response['access_token'],
+        :refresh_token => response['refresh_token']
+      }
+    end
   end
 end

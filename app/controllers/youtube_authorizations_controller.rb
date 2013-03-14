1dd4c1583042e9048db64b81c4d98e00c424f563
Ali Ibrahim <aibrahim2k2@gmail.com>
2013-03-13 22:02:47 -0400

    Retrieve access token when getting authorization
      * Need both refresh token and access token to regain authorization after
        access token has expired
      * Also use new Youtube::Client

--- app/controllers/youtube_authorizations_controller.rb
+++ app/controllers/youtube_authorizations_controller.rb
@@ -1,14 +1,15 @@
 class YoutubeAuthorizationsController < ApplicationController
   def create
-    youtube = Youtube::Authorization.new(Youtube::Client.new)
+    youtube = Youtube::Client.new
     redirect_to youtube.authorization_redirect_url
   end
 
   def show
-    youtube = Youtube::Authorization.new(Youtube::Client.new)
-    refresh_token = youtube.generate_refresh_token(params[:code])
-    flash[:notice] =
-      "Authorized! The refresh token is #{refresh_token}"
+    youtube = Youtube::Client.new
+    tokens = youtube.generate_tokens(params[:code])
+    flash[:notice] = "Authorized! " +
+      "Access token: #{tokens[:access_token]}. " +
+      "Refresh token: #{tokens[:refresh_token]}"
     redirect_to admin_path
   end
 end

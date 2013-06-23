bddf54fbef119d154d1de1b0460bfcb000d77ec5
Ali Ibrahim <aibrahim2k2@gmail.com>
2013-06-23 14:09:01 -0400

    Create application variable for admin_email
      * Send playlist update notifications to admin_email

--- config/initializers/admin_credentials.rb
+++ config/initializers/admin_credentials.rb
@@ -1,2 +1,3 @@
 BetterVideoPlaylist::Application.config.admin_username = ENV['better_video_playlist_username']
 BetterVideoPlaylist::Application.config.admin_password = ENV['better_video_playlist_password']
+BetterVideoPlaylist::Application.config.admin_email = ENV['better_video_playlist_email']

273b03292028513535398a5e17067b454c3d201d
Ali Ibrahim <aibrahim2k2@gmail.com>
2013-09-24 20:45:06 -0400

    Remove sensitive data
      * Remove secret token from config/initializers/secret_token.rb and regenerate
        new secret token stored in environment variable
      * Remove config/database.yml from git history and replace it with
        config/database.yml-example

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BetterVideoPlaylist::Application.config.secret_token = 'a1144b98bcdcb2a1845eaea3401b9aea56a30c31b0fbed500ac21daf44367adbafce0c968c2d5799cdefba0ef4f41e48087c4eb59c0956dfd63e648015076d34'
+BetterVideoPlaylist::Application.config.secret_token =
+  ENV['better_video_playlist_secret_token']
